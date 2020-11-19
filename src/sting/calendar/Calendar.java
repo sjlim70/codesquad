5aeda2a60acc2886ea42f869c3ae9cc15d719f27
sjlim70 <sjlim70@gmail.com>
2020-11-19 14:16:49 +0900

    Implement find max day

--- src/sting/calendar/Calendar.java
+++ src/sting/calendar/Calendar.java
@@ -1,5 +1,7 @@
 package sting.calendar;
 
+import java.util.Scanner;
+
 public class Calendar {
 
 	public static void main(String[] args) {
@@ -10,6 +12,32 @@ public class Calendar {
 		System.out.println("8  9 10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
+		
+		//숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
+		int InputValue;
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		//String strNum = scanner.next();
+		
+		InputValue = scanner.nextInt();
+		int[] maxDays = {31,28,31,30,31,30,31,31,30,31,30,31};
+		
+//		if(InputValue == 2)
+//		{
+//			System.out.println("28");
+//		}
+//		else if(InputValue == 1 ||InputValue == 3 ||InputValue == 5 ||InputValue == 7 
+//				||InputValue == 8 ||InputValue == 10 ||InputValue == 12)
+//		{
+//			System.out.println("31");
+//		}
+//		else if(InputValue == 4 ||InputValue == 6 ||InputValue == 9 ||InputValue == 11)
+//		{
+//			System.out.println("30");
+//		}
+		System.out.printf("%d 월은 %d 일까지 있습니다.\n", InputValue, maxDays[InputValue -1]);
+		scanner.close();
 
 	}
 
